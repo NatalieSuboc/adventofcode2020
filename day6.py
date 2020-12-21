0ef4392ea7af71608beb603ff4cfa2ca4caefa66
NatalieSuboc <nsuboc@gmail.com>
2020-12-21 02:09:43 -0800

    day 6 part 2

--- day6.py
+++ day6.py
@@ -10,3 +10,21 @@ def sum_q_count(file):
             print(unique_qs)
             total += len(unique_qs)
     return total
+
+def sum_shared_q_count(file):
+    total = 0
+    with open(file) as txt:
+        groups = txt.read().split("\n\n")
+        for group in groups:
+            lines = group.split('\n')
+            shared = set()
+            for char in lines[0]:
+                shared.add(char)
+            for i in range(1, len(lines)):
+                line = lines[i]
+                cur = set()
+                for char in line:
+                    cur.add(char)
+                shared = shared.intersection(cur)
+            total += len(shared)      
+    return total
